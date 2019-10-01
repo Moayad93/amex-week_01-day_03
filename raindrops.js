dcf9bcc26cc1b3719693d67eb1a9bcdf63ddf6da
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-10-01 19:08:43 +0300

    add my solution

--- raindrops.js
+++ raindrops.js
@@ -1,2 +1,14 @@
-// Your Answer below
+let number = 1755;
+let result = "";
 
+if (number % 3 === 0) {
+    result += "Pling";
+}
+if (number % 5 === 0) {
+    result += "Plang";
+}
+if (number % 7 === 0) {
+    result += "Plong";
+}
+
+console.log(result || number.toString());
